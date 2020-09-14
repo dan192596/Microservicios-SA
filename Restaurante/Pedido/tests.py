1ed170139988acee5b01499f3648043aa22da7f0
Joel Daniel Perez Camey <dperez@yetis.io>
2020-09-13 21:09:45 -0600

    Covertura y prueba unitaria

--- Restaurante/Pedido/tests.py
+++ Restaurante/Pedido/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Django Rest Framework
+from rest_framework.test import APIClient
+from rest_framework import status
+
+#Models
+from Pedido.models import Pedido
+
+class UserTestCase(TestCase):
+    """
+    def setUp(self):
+        pedido = Pedido(
+            cui='2994932450101'
+        )        
+        pedido.save()
+    """
+
+    def test_signup_user(self):
+        """Check if we can create an user"""        
+        client = APIClient()
+        response = client.post(
+                '/api/v1/pedido', {
+                'cui': '2994932450101'
+            },
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
